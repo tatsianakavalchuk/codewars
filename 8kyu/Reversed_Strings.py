a9fe28e23f293d4ab838621d3e4bc4125e3b9f09
tatsianakavalchuk <125137988+tatsianakavalchuk@users.noreply.github.com>
2023-09-17 00:19:49 +0300

    init

--- 8kyu/Reversed_Strings.py
+++ 8kyu/Reversed_Strings.py
@@ -1,8 +1,8 @@
-# Complete the solution so that it reverses the string passed into it.
+#  Complete the solution so that it reverses the string passed into it.
 
-# 'world'  =>  'dlrow'
-# 'word'   =>  'drow'
+#  'world'  =>  'dlrow'
+#  'word'   =>  'drow'
 
-# My solution
+#  My solution
 def solution(string):
     return string[::-1]
